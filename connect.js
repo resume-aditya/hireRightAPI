41d15f7cb3b8b52273eeaf58bd16f2eb8845a91b
Aditya Sharma <a.aditya.s.sharma@oracle.com>
2021-09-21 00:31:52 +0530

    login logic

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 var cors = require('cors');
 const app = express();
 const { MongoClient } = require("mongodb");
@@ -6,12 +6,10 @@ const { MongoClient } = require("mongodb");
 app.use(cors());
 app.use(express.static("public"));
 
-const url = "mongodb+srv://db:nikita19@allserver.t6epw.mongodb.net/hiring?retryWrites=true&w=majority";
-// const client = new MongoClient(url);
-
+const mongo_url = "mongodb+srv://db:nikita19@allserver.t6epw.mongodb.net/hiring?retryWrites=true&w=majority";
 
 app.get("/api/questions", async function (req, res) {
-    const client = new MongoClient(url, { useUnifiedTopology: true });
+    const client = new MongoClient(mongo_url, { useUnifiedTopology: true });
     try {
       await client.connect();
   
@@ -28,7 +26,43 @@ app.get("/api/questions", async function (req, res) {
       await client.close();
     }
   });
-  
+  app.get("/login/userExists", async (req, res) => {
+    const client = new MongoClient(mongo_url, { useUnifiedTopology: true });
+    try {
+      await client.connect();
+      let username = req.query.username;
+      const database = client.db('users');
+      const collection = database.collection('userdata');
+      const users = await collection.find({username : username}).next();
+      const userExists = !!users;
+      return res.json(userExists);
+    } catch(err) {
+      console.log(err);
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  });
+  app.get("/login/doLogin", async (req, res) => {
+    const client = new MongoClient(mongo_url, { useUnifiedTopology: true });
+    try {
+      await client.connect();
+      let username = req.query.username;
+      let password = req.query.password;
+      const database = client.db('users');
+      const collection = database.collection('userdata');
+      const users = await collection.find({username : username, password : password}).next();
+      const userExists = !!users;
+      return res.json(userExists);
+    } catch(err) {
+      console.log(err);
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  });
   // start the server listening for requests
   app.listen(process.env.PORT || 3000, 
       () => console.log("Server is running..."));
